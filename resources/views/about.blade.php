75a50003de3fb7cfe33400ec3aff4c7639c401e3
Alex SaM <alsam3637@gmail.com>
2025-04-04 13:47:33 -0400

    Add about template and route. Config publish broadcasting

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello world!!!</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium aperiam consectetur corporis distinctio doloremque enim esse fugiat illum incidunt laboriosam magnam maxime neque nostrum optio, qui recusandae reprehenderit sequi.</p>
