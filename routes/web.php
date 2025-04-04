358652f5e015543cb70c1c931e852417f9a36e7a
Alex SaM <alsam3637@gmail.com>
2025-04-04 15:24:54 -0400

    Create CarController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::view('/about', 'about')->name('about');
+
+Route::get('/car', [CarController::class, 'index'])->name('car.index');
