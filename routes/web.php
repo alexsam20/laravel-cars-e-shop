7397876e8e8b8db93093d180192ec24a17dd6e65
Alex SaM <alsam3637@gmail.com>
2025-04-05 15:01:00 -0400

    Create LoginController and login blade.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginComtroller;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/signup', [SignupController::class, 'create']);
+Route::get('/login', [LoginComtroller::class, 'create']);
