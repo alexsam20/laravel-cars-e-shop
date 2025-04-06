a4e201352b2fb64e96cba7367f050879676b332e
Alex SaM <alsam3637@gmail.com>
2025-04-06 09:22:46 -0400

    Clear templates. Delete footer link,

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,4 @@
 <x-app-layout title="Home Page">
-    @php
-        $color = 'red';
-        $bgColor = 'blue';
-    @endphp
-    <x-card :$color :$bgColor class="card-rounded">
-        <x-slot:title class="card-header-blue">Card title 1</x-slot>
-        Card Content 1
-        <x-slot:footer>Card footer 1</x-slot>
-    </x-card>
-    <x-test-component class="card">
-        Lorem ipsum dolor sit amet
-    </x-test-component>
-
     <!-- Home Slider -->
     <section class="hero-slider">
         <!-- Carousel wrapper -->
@@ -116,8 +103,4 @@
         </section>
         <!--/ New Cars -->
     </main>
-    <x-slot:footerLinks>
-        <a href="#">Link 3</a>
-        <a href="#">Link 4</a>
-    </x-slot:footerLinks>
 </x-app-layout>
