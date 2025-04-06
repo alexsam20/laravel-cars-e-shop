caa7d1c15e79eba2826d7a303594216750b1f27d
Alex SaM <alsam3637@gmail.com>
2025-04-06 18:19:00 -0400

    Add $timestamps in Models.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class Model extends EloquentModel
 {
-    //
+    use HasFactory;
+
+    public $timestamps = false;
 }
