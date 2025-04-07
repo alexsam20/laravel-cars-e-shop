b2e9a8ada0f217f1d9f5a41bc13dd96dd6a92f04
Alex SaM <alsam3637@gmail.com>
2025-04-07 18:11:09 -0400

    Car Model hasMany.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use App\Models\CarFeatures;
+use App\Models\CarImage;
 use App\Models\FuelType;
 use App\Models\Maker;
 
@@ -20,9 +21,9 @@ class HomeController extends Controller
 //        $car->features->update(['abc'=> 0]);
 //        $car->primaryImage->delete();
 
-        $car = Car::find(11);
+//        $car = Car::find(11);
 
-        $carFeatures = new CarFeatures([
+        /*$carFeatures = new CarFeatures([
             'abc' => false,
             'air_conditioning' => false,
             'power_windows' => false,
@@ -35,10 +36,27 @@ class HomeController extends Controller
             'climate_control' => false,
             'rear_parking_sensor' => false,
             'leather_seats' => false,
+        ]);*/
+
+//        $car->features()->save($carFeatures);
+
+        $car = Car::find(1);
+
+        // Create new image
+//        $image = new CarImage(['image_path' => 'me_folder/my_subfolder/myy_file', 'position' => 2]);
+//        $car->images()->save($image);
+
+//        $car->images()->create(['image_path' => 'folder/subfolder/file', 'position' => 3]);
+
+        $car->images()->saveMany([
+            new CarImage(['image_path' => 'folder2/subfolder2/file2', 'position' => 4]),
+            new CarImage(['image_path' => 'folder3/subfolder3/file3', 'position' => 5]),
         ]);
 
-        $car->features()->save($carFeatures);
-
+        $car->images()->createMany([
+            ['image_path' => 'folder4/subfolder4/file4', 'position' => 6],
+            ['image_path' => 'folder5/subfolder5/file5', 'position' => 7],
+        ]);
 
         return view('home.index');
     }
--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -38,6 +39,11 @@ class Car extends Model
         return $this->hasOne(CarImage::class)
 //            ->latestOfMany('position');
             ->oldestOfMany('position');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(CarImage::class);
     }
 
 }
