83ac95416ac3bceb9af2c050afe9c5e8ec4236c2
Alex SaM <alsam3637@gmail.com>
2025-04-07 11:23:25 -0400

    CRUD.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,98 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
+use App\Models\FuelType;
+use App\Models\Maker;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // Select All Cars
-//        $cars = Car::get();
+        // Challenge: Rewrite all Cars records where th price is greater than $20 000
+        $cars = Car::where('price', '>', 20000)->get();
+        dump($cars);
 
-        // Select Published Cars
-//        $cars = Car::where('published_at', "!=", null)->get();
+        // Challenge: Fetch the Maker details where the Maker name is 'Toyota'
+        $maker = Maker::where('name', 'Toyota')->first();
+        dump($maker);
 
-        // Select the first car
-//        $car = Car::where('published_at', "!=", null)->first();
+        // Challenge: Insert a new FuelType with the name 'Hydrogen'
+        FuelType::create(['name' => 'Hydrogen']);
 
-//        $car = Car::find(3);
+        // Challenge: Update the price of the Car with id 1 to $15 000
+        Car::where('id', 1)->update(['price' => 15000]);
 
-//        $car = Car::where('published_at', "!=", null)
-//            ->where('user_id', 1)
-//            ->orderBy('published_at', 'desc')
-//            ->limit(2)
-//            ->get();
-
-//        $carDate = [
-//            'maker_id' => 1,
-//            'model_id' => 1,
-//            'year' => 2024,
-//            'price' => 20000,
-//            'vin' => 999,
-//            'mileage' => 5000,
-//            'car_type_id' => 1,
-//            'fuel_type_id' => 1,
-//            'user_id' => 1,
-//            'city_id' => 1,
-//            'address' => 'PO BOX 888',
-//            'phone' => '5554448800',
-//            'description' =>  null,
-//            'published_at' => now(),
-//        ];
-
-        // Approach 1
-//        $car1 = Car::create($carDate);
-
-        // Approach 2
-//        $car2 = new Car();
-//        $car2->fill($carDate);
-//        $car2->save();
-
-        // Approach 3
-//        $car3 = new Car($carDate);
-//        $car3->save();
-
-//        $car = Car::find(1);
-//        $car->price = 100000;
-//        $car->save();
-
-//        $carDate = [
-//            'maker_id' => 1,
-//            'model_id' => 1,
-//            'year' => 2024,
-//            'price' => 20000,
-//            'vin' => 9999,
-//            'mileage' => 5000,
-//            'car_type_id' => 1,
-//            'fuel_type_id' => 1,
-//            'user_id' => 1,
-//            'city_id' => 1,
-//            'address' => 'PO BOX 888',
-//            'phone' => '5554448800',
-//            'description' =>  null,
-//            'published_at' => now(),
-//        ];
-
-//        Car::updateOrCreate(
-//            ['vin' => '9999', 'price' => 20000],
-//            $carDate
-//        );
-
-//        Car::where('published_at', null)
-//            ->where('user_id', 1)
-//            ->update(['published_at' => now()]);
-
-//        $car = Car::find(1); // Second time Error
-//        $car->delete();
-
-//        Car::destroy(2, 3);
-
-//        Car::where('published_at', null)
-//            ->where('user_id', 1)
-//            ->delete();
-
-//        Car::truncate();
-
+        // Challenge: Delete all Car records where the year is before 2020
+        Car::where('year', '<', 2020)->delete();
         return view('home.index');
     }
 }
