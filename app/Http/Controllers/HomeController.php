63e3909bb2643af4d8a8a27dd99846886bd35b34
Alex SaM <alsam3637@gmail.com>
2025-04-07 10:00:43 -0400

    Testing ORM.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,77 @@ class HomeController extends Controller
 //            ->orderBy('published_at', 'desc')
 //            ->limit(2)
 //            ->get();
-//
-//        dump($car);
+
+//        $carDate = [
+//            'maker_id' => 1,
+//            'model_id' => 1,
+//            'year' => 2024,
+//            'price' => 20000,
+//            'vin' => 999,
+//            'mileage' => 5000,
+//            'car_type_id' => 1,
+//            'fuel_type_id' => 1,
+//            'user_id' => 1,
+//            'city_id' => 1,
+//            'address' => 'PO BOX 888',
+//            'phone' => '5554448800',
+//            'description' =>  null,
+//            'published_at' => now(),
+//        ];
+
+        // Approach 1
+//        $car1 = Car::create($carDate);
+
+        // Approach 2
+//        $car2 = new Car();
+//        $car2->fill($carDate);
+//        $car2->save();
+
+        // Approach 3
+//        $car3 = new Car($carDate);
+//        $car3->save();
+
+//        $car = Car::find(1);
+//        $car->price = 100000;
+//        $car->save();
+
+//        $carDate = [
+//            'maker_id' => 1,
+//            'model_id' => 1,
+//            'year' => 2024,
+//            'price' => 20000,
+//            'vin' => 9999,
+//            'mileage' => 5000,
+//            'car_type_id' => 1,
+//            'fuel_type_id' => 1,
+//            'user_id' => 1,
+//            'city_id' => 1,
+//            'address' => 'PO BOX 888',
+//            'phone' => '5554448800',
+//            'description' =>  null,
+//            'published_at' => now(),
+//        ];
+
+//        Car::updateOrCreate(
+//            ['vin' => '9999', 'price' => 20000],
+//            $carDate
+//        );
+
+//        Car::where('published_at', null)
+//            ->where('user_id', 1)
+//            ->update(['published_at' => now()]);
+
+//        $car = Car::find(1); // Second time Error
+//        $car->delete();
+
+//        Car::destroy(2, 3);
+
+//        Car::where('published_at', null)
+//            ->where('user_id', 1)
+//            ->delete();
+
+//        Car::truncate();
+
         return view('home.index');
     }
 }
