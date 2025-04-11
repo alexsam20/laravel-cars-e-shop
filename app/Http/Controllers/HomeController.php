df4b85a1e410176855ddd6a2b73d86be65b39255
Alex SaM <alsam3637@gmail.com>
2025-04-11 17:00:03 -0400

    Creating Response.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,16 @@ class HomeController extends Controller
 {
     public function index()
     {
+//        return Car::get();
+//        return Car::first();
+        /*return response('Hello World', 200)
+            ->header('X-Developer', 'AlexSam')
+            ->header('X-Developer-Date', '2025-04-04 - '.date('Y-m-d'));
+            ;*/
+//        return response()->json([1,2]);
+        /*return response()->view('view', [], 200)
+            ->withHeaders(['X-Developer' => 'AlexSam']);*/
+
         $cars = Car::where('published_at', '<', now())
             ->with(['primaryImage', 'city', 'carType', 'fuelType', 'maker', 'model'])
             ->orderBy('published_at', 'desc')
