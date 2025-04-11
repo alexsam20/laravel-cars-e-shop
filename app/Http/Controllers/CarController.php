ef8ca51c970e960a8e75b776c721b758efcea7c1
Alex SaM <alsam3637@gmail.com>
2025-04-11 14:19:51 -0400

    Accessing the Request.

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -11,22 +11,14 @@ class CarController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+//        dd($request, request());
         $cars = User::find(1)
             ->cars()
             ->with(['primaryImage', 'maker', 'model'])
             ->orderBy('created_at', 'desc')
-            ->paginate(5)
-            // http://127.0.0.1:8000/car?page=2
-//            ->withPath('/user/cars')
-            // http://127.0.0.1:8000/user/cars?page=2
-//            ->appends(['sort' => 'price'])
-//            ->withQueryString()
-            // http://127.0.0.1:8000/car?sort=price&page=3
-//            ->fragment('cars')
-            // http://127.0.0.1:8000/car?page=3#cars
-        ;
+            ->paginate(15);
 
         return view('car.index', compact('cars'));
     }
@@ -50,8 +42,47 @@ class CarController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Car $car)
+    public function show(Request $request, Car $car)
     {
+        // URL
+        // http://127.0.0.1:8000/car/1?page=1 ***************
+        dump($request->path()); // Output: car/1
+        dump($request->url()); // Output: http://127.0.0.1:8000/car/1
+        dump($request->fullUrl()); // Output: http://127.0.0.1:8000/car/1?page=1
+        dump($request->method()); // Output: GET
+
+        if ($request->isMethod('post')) {
+            // POST Request Method
+        }
+        if ($request->isXmlHttpRequest()) {
+            // AJAX Request
+        }
+        if ($request->is('admin/*')) {
+            // Request URL matches the pattern admin/
+        }
+        if ($request->routeIs('admin.*')) {
+            // Request URL matches thr pattern admin.*
+        }
+        if ($request->expectsJson()) {
+            // Request except JSON response
+        }
+        dump($request->fullUrlWithQuery(['sort' => 'price']));
+        // Output: http://127.0.0.1:8000/car/1?page=1&sort=price
+        dump($request->fullUrlWithoutQuery(['page']));
+        // Output: http://127.0.0.1:8000/car/1
+        dump($request->host());
+        // Output: 127.0.0.1
+        dump($request->httpHost());
+        // Output: 127.0.0.1:8000
+        dump($request->schemeAndHttpHost());
+        // Output: http://127.0.0.1:8000
+        dump($request->header('Content-Type'));
+        // Output: null
+        dump($request->bearerToken());
+        // Output: null
+        dump($request->ip());
+        // Output: 127.0.0.1
+
         return view('car.show', compact('car'));
     }
 
